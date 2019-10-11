981ee345dbc1e6ee6493a1f92a1c60e3dc10169b
kyle <aizuguo_@126.com>
2019-10-11 21:39:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ DIR=`S=\`readlink "$0"\`; [ -z "$S" ] && S=$0; dirname $S`
 echo $DIR
 
 # optional
-# cd /usr/local/bin && ln -s /full_path/start.sh vpn
+# ln -s /u02/app/vpn/start.sh /usr/local/bin/vpn
 
 sshuttle --dns -vr opc@x.x.x.x 0/0 --ssh-cmd "ssh -i ${DIR}/id_rsa"
