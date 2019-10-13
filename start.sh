49e81cfb21c04ffc662c081037f185dd71b6dcc1
kyle <aizuguo_@126.com>
2019-10-13 17:37:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ echo $DIR
 
 chmod 400 ${DIR}/id_rsa
 sshuttle --dns -vr opc@x.x.x.x 0/0 --ssh-cmd "ssh -i ${DIR}/id_rsa"
+# ubuntu 18
+sshuttle --dns -vr opc@x.x.x.x 0/0 -x x.x.x.x --ssh-cmd "ssh -i ${DIR}/id_rsa"
+
 
 # optional
 # ln -s /u02/app/vpn/start.sh /usr/local/bin/vpn
